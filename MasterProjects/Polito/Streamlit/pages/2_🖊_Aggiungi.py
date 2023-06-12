44b931dd297a341b3a365397fc1d01c8e193880c
DavideCarletto <102655420+DavideCarletto@users.noreply.github.com>
2023-06-13 00:28:07 +0200

    Initial Commit

--- MasterProjects/Polito/Streamlit/pages/2_🖊_Aggiungi.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from utils.utils import *
-
-if __name__ == "__main__":
-    st.title("🖊 Aggiungi")
--- MasterProjects/Polito/Streamlit/utils/utils.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-from sqlalchemy import create_engine,text
-
-"""Raccoglie le principali funzioni condivise dalle varie pagine"""
